6a4863f0fcc3b5a4814c8e207990863093bd4761
newbi462 <isthisemailfree@gmail.com>
2019-11-10 13:20:13 -0500

    Inside of your App.js file import { Route } from 'react-router-dom';

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React from 'react';
 import './App.css';
 import { Home, About, Contact, Navigation } from './components';
 
+// Inside of your App.js file import { Route } from 'react-router-dom';
+import { Route } from 'react-router-dom';
+
 const App = () => (
   <div>
     <Navigation />
